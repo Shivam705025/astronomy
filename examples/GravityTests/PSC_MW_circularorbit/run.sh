a2e4fa3b2b4c68db36952c1eed3dfeb096190e80
Darwin Roduit <darwin.roduit@epfl.ch>
2023-03-17 17:06:03 +0100

    Rewritten the file to delete the outputs before running again the simulation

--- examples/GravityTests/PSC_MW_circularorbit/run.sh
+++ examples/GravityTests/PSC_MW_circularorbit/run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ ! -e circularorbitshernquist.hdf5 ]
-then 
-    echo "Generate initial conditions for circular orbits"
-    if command -v python3 &>/dev/null; then
-        python3 makeIC.py
-    else 
-        python3 makeIC.py
-    fi
-
+rm output_*
+rm xyplot.png xzplot.png yzplot.png
+# if [ ! -e circular_orbits_MW.hdf5 ]
+# then
+echo "Generate initial conditions for circular orbits"
+if command -v python3 &>/dev/null; then
+    python3 makeIC.py
+else
+    python3 makeIC.py
 fi
 
-# self gravity G, external potential g, hydro s, threads t and high verbosity v
-../../../swift --external-gravity --threads=6 hernquistcirc.yml 2>&1 | tee output.log
+# fi
 
+# self gravity G, external potential g, hydro s, threads t and high verbosity v
+../../../swift --external-gravity --threads=8 nfw_mn_psc_circular_orbits.yml 2>&1 | tee output.log
 
 echo "Save plots of the circular orbits"
 if command -v python3 &>/dev/null; then
